72ec870332a059087c9b802b2456c4caa7013cbd
tleonchenko <tl@elpicom.com>
2013-06-11 16:54:01 +0300

    2755

---
+++ js/modules/m-subnavigation.js
@@ -0,0 +1,8 @@
+$(function(){
+    alert('dfsdfds');
+    $('.m-subnavigation-item').click(function(){
+        /*$(this).toggleClass("active");*/
+    });
+})
+        /*
+    $('.m-subnavigation-item-best')*/
