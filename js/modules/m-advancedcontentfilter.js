b4c2602141bd6a8c620b19c6939dfca4199391a3
tleonchenko <tl@elpicom.com>
2013-07-04 17:47:59 +0300

    IMHONET-2963

---
+++ js/modules/m-advancedcontentfilter.js
@@ -0,0 +1,59 @@
+define(
+    [
+        "app",
+        "layouts/filter",
+        "layouts/filter-current",
+        "models/filter.model"
+    ],
+
+    function(App, FilterLayout, FilterCurrentLayout, FilterModel){
+
+        App.module('AdvancedContentFilter', function(AdvancedContentFilter){
+
+            var filterRegion = new Marionette.Region({
+                el : '[data-region="m-advancedcontentfilter"]'
+            })
+            var filterCurrentRegion = new Marionette.Region({
+                el : '[data-region="m-advancedcontentfilter-current"]'
+            })
+
+            var filterModel = new FilterModel();
+
+            var Controller = Marionette.Controller.extend({
+
+                filterSyncedState : function() {
+
+                    this.listenToOnce(filterModel,"sync",this.showLayout);
+                    return this;
+                },
+
+                fetchFilter : function() {
+
+                    filterModel.fetch();
+                    return this;
+                },
+
+                showLayout : function() {
+
+                    filterRegion.show(new FilterLayout({
+                        model : filterModel
+                    }));
+
+                    filterCurrentRegion.show(new FilterCurrentLayout({
+                        model : filterModel
+                    }))
+
+                    return this;
+                }
+            })
+
+            AdvancedContentFilter.controller = new Controller();
+
+            AdvancedContentFilter.addInitializer(function(){
+
+                this.controller.filterSyncedState().fetchFilter();
+            })
+
+        });
+    }
+);
